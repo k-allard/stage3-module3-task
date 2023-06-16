10f342994822ec64ff06afc07118b3dc462af5bd
Anastasiia Smirnova <smirn.anastasiia@gmail.com>
2023-06-16 23:23:17 +0400

    Add unit tests for NewsRequestDtoValidator

--- module-service/src/test/java/com/mjc/school/service/validator/NewsRequestDtoValidatorTest.java
+++ module-service/src/test/java/com/mjc/school/service/validator/NewsRequestDtoValidatorTest.java
@@ -5,20 +5,20 @@ import com.mjc.school.repository.impl.NewsRepository;
 import com.mjc.school.service.dto.NewsRequestDto;
 import com.mjc.school.service.exceptions.NotFoundException;
 import com.mjc.school.service.exceptions.ValidatorException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.lenient;
 
 @ExtendWith(MockitoExtension.class)
-class NewsRequestDtoValidatorTest {
+public class NewsRequestDtoValidatorTest {
 
     private static final long VALID_NEWS_ID = 3L;
     private static final long VALID_AUTHOR_ID = 2L;
@@ -31,45 +31,45 @@ class NewsRequestDtoValidatorTest {
             "is sensationalism, the disproportionate focus on, and exaggeration of, emotive stories for public " +
             "consumption. This news is also not unrelated to gossip, the human practice of sharing information.";
 
-    @Mock
-    private NewsRepository newsRepository;
-
-    @Mock
-    private AuthorRepository authorRepository;
-
-    @InjectMocks
     private NewsRequestDtoValidator validator;
 
+    @BeforeEach
+    void init(@Mock NewsRepository newsRepository,
+              @Mock AuthorRepository authorRepository) {
+        validator = new NewsRequestDtoValidator(authorRepository, newsRepository);
+        lenient().when(newsRepository.existById(VALID_NEWS_ID))
+                .thenReturn(true);
+        lenient().when(authorRepository.existById(VALID_AUTHOR_ID))
+                .thenReturn(true);
+    }
 
     @Test
     @DisplayName("validateNewsId() with existent id - OK")
-    void getNewsByValidId() {
-        Mockito.when(newsRepository.existById(VALID_NEWS_ID))
-                .thenReturn(true);
+    public void getNewsBy1ValidId() {
         assertDoesNotThrow(() ->
                 validator.validateNewsId(VALID_NEWS_ID));
     }
 
     @Test
     @DisplayName("validateNewsId() with non-existent id fails")
-    void getNewsByInvalidId() {
+    public void getNewsByInvalidId() {
         NotFoundException thrown = assertThrows(NotFoundException.class, () ->
                 validator.validateNewsId(INVALID_NEWS_ID));
-        assertTrue(thrown.getMessage().contains("News with id %d does not exist".formatted(INVALID_NEWS_ID)));
+        assertTrue(thrown.getMessage().contains(
+                "News with id %d does not exist".formatted(INVALID_NEWS_ID)
+        ));
     }
 
     @Test
     @DisplayName("validateAuthorId() with existent id - OK")
-    void createNewsWithValidAuthorId() {
-        Mockito.when(authorRepository.existById(VALID_AUTHOR_ID))
-                .thenReturn(true);
+    public void createNewsWithValidAuthorId() {
         assertDoesNotThrow(() ->
                 validator.validateAuthorId(VALID_AUTHOR_ID));
     }
 
     @Test
     @DisplayName("validateAuthorId() with non-existent id fails")
-    void createNewsWithInvalidAuthorId() {
+    public void createNewsWithInvalidAuthorId() {
         NotFoundException thrown = assertThrows(NotFoundException.class, () ->
                 validator.validateAuthorId(INVALID_AUTHOR_ID));
         assertTrue(thrown.getMessage().contains("Author Id does not exist"));
@@ -77,24 +77,28 @@ class NewsRequestDtoValidatorTest {
 
     @Test
     @DisplayName("validateNewsDTORequest() with valid NewsRequestDto - OK")
-    void createNewsWithValidDTO() {
+    public void createNewsWithValidDTO() {
         assertDoesNotThrow(() ->
                 validator.validateNewsDTORequest(
-                        new NewsRequestDto(null, VALID_NEWS_TITLE, VALID_NEWS_CONTENT, VALID_AUTHOR_ID)));
+                        new NewsRequestDto(
+                                null, VALID_NEWS_TITLE, VALID_NEWS_CONTENT, VALID_AUTHOR_ID)
+                ));
     }
 
     @Test
     @DisplayName("validateNewsDTORequest() with invalid title fails")
-    void createNewsWithInvalidTitle() {
+    public void createNewsWithInvalidTitle() {
         ValidatorException thrown = assertThrows(ValidatorException.class, () ->
                 validator.validateNewsDTORequest(
-                        new NewsRequestDto(null, INVALID_NEWS_TITLE, VALID_NEWS_CONTENT, VALID_AUTHOR_ID)));
+                        new NewsRequestDto(
+                                null, INVALID_NEWS_TITLE, VALID_NEWS_CONTENT, VALID_AUTHOR_ID)
+                ));
         assertTrue(thrown.getMessage().contains("News title can not be"));
     }
 
     @Test
     @DisplayName("validateNewsDTORequest() with invalid content fails")
-    void createNewsWithInvalidContent() {
+    public void createNewsWithInvalidContent() {
         ValidatorException thrown = assertThrows(ValidatorException.class, () ->
                 validator.validateNewsDTORequest(
                         new NewsRequestDto(
