aeb4f8bf8da80883dcf1596b260af35921d32b0c
Anastasiia Smirnova <smirn.anastasiia@gmail.com>
2023-06-16 23:49:59 +0400

    Reformat some code

--- module-service/src/main/java/com/mjc/school/service/exceptions/ExceptionsCodes.java
+++ module-service/src/main/java/com/mjc/school/service/exceptions/ExceptionsCodes.java
@@ -1,21 +1,21 @@
 package com.mjc.school.service.exceptions;
 
 public enum ExceptionsCodes {
-  NEWS_ID_DOES_NOT_EXIST( "01", "News with id %d does not exist."),
-  AUTHOR_ID_DOES_NOT_EXIST("02", "Author Id does not exist. Author Id is: %s"),
-  VALIDATE_NEGATIVE_OR_NULL_NUMBER(
-          "03", "%s can not be null or less than 1. %s is: %s"),
-  VALIDATE_STRING_LENGTH(
-          "04", "%s can not be less than %d and more than %d symbols. %s is %s"),
-  VALIDATE_INT_VALUE("05", "%s should be number");
+    NEWS_ID_DOES_NOT_EXIST("01", "News with id %d does not exist."),
+    AUTHOR_ID_DOES_NOT_EXIST("02", "Author Id does not exist. Author Id is: %s"),
+    VALIDATE_NEGATIVE_OR_NULL_NUMBER(
+            "03", "%s can not be null or less than 1. %s is: %s"),
+    VALIDATE_STRING_LENGTH(
+            "04", "%s can not be less than %d and more than %d symbols. %s is %s"),
+    VALIDATE_INT_VALUE("05", "%s should be number");
 
-  private final String errorMessage;
+    private final String errorMessage;
 
-  ExceptionsCodes(String errorCode, String message) {
-    this.errorMessage = "ERROR_CODE: " + errorCode + " ERROR_MESSAGE: " + message;
-  }
+    ExceptionsCodes(String errorCode, String message) {
+        this.errorMessage = "ERROR_CODE: " + errorCode + " ERROR_MESSAGE: " + message;
+    }
 
-  public String getMessage() {
-    return errorMessage;
-  }
+    public String getMessage() {
+        return errorMessage;
+    }
 }
